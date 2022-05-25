2caf63ce6d60694c2c5d3b911310baee874aef57
ayan2809 <ayansadhukhan28@gmail.com>
2022-05-25 11:30:35 +0530

    URL Change

--- GenZ_Ed/settings.py
+++ GenZ_Ed/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t@obibolb_!%%e-cy7jlacmzl--hu0d&xd%ddopcc1+26nbj0)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'genz-ed.herokuapp.com']
 
 
 # Application definition
