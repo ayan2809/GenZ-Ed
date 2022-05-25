116b207cef67deb353c328ff5f58a2b3119791d5
ayan2809 <ayansadhukhan28@gmail.com>
2022-05-25 11:14:18 +0530

    Updated

--- GenZ_Ed/urls.py
+++ GenZ_Ed/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf.urls import url
+from django.views.static import serve
+from django.conf import settings
+from django.urls import include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = 'index'),
+    # path('', include("Home.urls")),
+
+      url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+
