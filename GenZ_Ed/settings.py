9af2447e394abff89df378aea068723c5573a847
ayan2809 <ayansadhukhan28@gmail.com>
2022-05-25 13:27:49 +0530

    Static files

--- GenZ_Ed/settings.py
+++ GenZ_Ed/settings.py
@@ -116,11 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 import os
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # print(STATIC_ROOT)
 
 # Default primary key field type
