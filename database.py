4994222ae0b4daac02d201c682c6a8d0dd276fd0
ayan2809 <ayansadhukhan28@gmail.com>
2022-05-27 10:14:09 +0530

    Added Gitignore

--- database.py
+++ database.py
@@ -4,9 +4,13 @@ from pymongo import MongoClient
 client =Connect.get_connection()
 
 def insertTeacherData(username, firstname, lastname, email, password):
-    cursor = client.teacher.users.count_documents({"username": username})
-    # print(cursor)
-    if cursor == 0:
+    # query to check if the database has a particular username
+    query1 = client.teacher.users.count_documents({"username": username})
+    # query to check if the database has a particular email
+    query2 = client.teacher.users.count_documents({"email": email})
+
+    # if both of them are zero then only proceed to enter into the db
+    if query1 == 0 and query2 == 0: 
         client.teacher.users.insert_one({"username": username, "firstname": firstname, "lastname": lastname, "email": email, "password": password})
         print("Inserted")
         return True
@@ -15,11 +19,19 @@ def insertTeacherData(username, firstname, lastname, email, password):
     return False
 
 def insertStudentData(username, firstname, lastname, email, password):
-    cursor = client.student.users.count_documents({"username": username})
-    if cursor == 0:
+    # query to check if the database has a particular username
+    query1 = client.student.users.count_documents({"username": username})
+    # query to check if the database has a particular username
+    query2 = client.student.users.count_documents({"email": email})
+
+    # if both of them are zero then only proceed to enter into the db
+    if query1 == 0 and query2 == 0:
         client.student.users.insert_one({"username": username, "firstname": firstname, "lastname": lastname, "email": email, "password": password})
         print("Inserted")
         return True
     
     print("Student already Exists")
     return False
+
+def signInAuthentication(username, email, password):
+    return True
